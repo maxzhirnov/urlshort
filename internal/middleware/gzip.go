a0c6a56f39a0165cc5b5d8d6a4e0979eb479f8fb
mzhirnov <maximz2009@gmail.com>
2023-08-21 22:49:28 +0300

    fixes

--- internal/middleware/gzip.go
+++ internal/middleware/gzip.go
@@ -28,23 +28,14 @@ func GzipMiddleware() gin.HandlerFunc {
 			c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 		}
 
-		gw := gzip.NewWriter(c.Writer)
-		defer gw.Close()
-
-		// Прихраним оригинальный writer
-		originalWriter := c.Writer
-		c.Writer = &gzipResponseWriter{gw, originalWriter}
-
 		c.Next()
 
 		contentType := c.Writer.Header().Get("Content-Type")
-		if strings.HasPrefix(contentType, "application/json") || strings.HasPrefix(contentType, "text/html") {
-			if strings.Contains(c.GetHeader("Accept-Encoding"), "gzip") {
-				c.Writer.Header().Set("Content-Encoding", "gzip")
-			}
-		} else {
-			// Если Content-Type не json или html, восстанавливаем оригинальный writer
-			c.Writer = originalWriter
+		if (strings.HasPrefix(contentType, "application/json") || strings.HasPrefix(contentType, "text/html")) && strings.Contains(c.GetHeader("Accept-Encoding"), "gzip") {
+			gw := gzip.NewWriter(c.Writer)
+			defer gw.Close()
+			c.Writer = &gzipResponseWriter{gw, c.Writer}
+			c.Writer.Header().Set("Content-Encoding", "gzip")
 		}
 	}
 }
